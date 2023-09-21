6bb85e59d7317dc42c7840c417ea78e4774623e8
Daniel Turinn <turindani@gmail.com>
2023-09-21 13:47:46 +0300

    reverting changes

--- collector_client/collector_client.go
+++ collector_client/collector_client.go
@@ -5,7 +5,6 @@ import (
 	"encoding/json"
 	"io"
 	"os"
-	"sync"
 	"time"
 
 	"github.com/danielturin/evm-event-collector/client"
@@ -21,14 +20,18 @@ type Collector interface {
 
 type collector struct {
 	CollectorClient client.Client
-	Reactor         reactor.Reactor[types.LogEvent, types.Callback]
 }
 
-func New() *collector {
+func Start(addr string, timeout_duration int64) *collector {
 	logger.CreateLoggerInstance()
 	log := logger.GetNamedLogger("collector_client")
 
 	defer logger.Sync()
+
+	if len(addr) == 0 {
+		log.Error("Please enter a valid websocket SOCKET_ADDRS in .env")
+		return nil
+	}
 
 	contract_config, err := os.Open("./config.json")
 	if err != nil {
@@ -62,72 +65,22 @@ func New() *collector {
 		_ = reactor.Start(ctx)
 	}()
 
-	c := client.New(reactor, contractData)
-	cc := &collector{
-		CollectorClient: *c,
-		Reactor:         reactor,
-	}
-	return cc
-}
-
-func (col *collector) Start(addr string, timeout_duration int64) {
-	logger.CreateLoggerInstance()
-	log := logger.GetNamedLogger("collector_client")
-
-	defer logger.Sync()
-
-	if len(addr) == 0 {
-		log.Error("Please enter a valid websocket SOCKET_ADDRS in .env")
-		return
-	}
-
-	contract_config, err := os.Open("./config.json")
-	if err != nil {
-		log.Sugar().Errorf("Could not open config.json: ", err)
-		panic(err)
-	}
-	defer contract_config.Close()
-
-	// Read the contents of the file
-	data, err := io.ReadAll(contract_config)
-	if err != nil {
-		log.Sugar().Errorf("Could not read config.json: ", err)
-		panic(err)
-	}
-
-	// Parse the JSON data into the ContractData struct
-	var contractData types.ContractData
-	err = json.Unmarshal(data, &contractData)
-	if err != nil {
-		log.Sugar().Errorf("Could not parse contract data from config.json: ", err)
-		panic(err)
-	}
-
-	ctx, cancel := context.WithCancel(context.Background())
-	defer cancel()
-
 	if timeout_duration == 0 {
 		timeout_duration = 100000 // default fallback timeout
 	}
 	timeout := time.Duration(timeout_duration) * time.Millisecond
-	var wg1 sync.WaitGroup
-	wg1.Add(1)
-	go func() {
-		col.CollectorClient.Subscriber.Connect(ctx, addr, timeout)
-		if err != nil {
-			log.Error("failed to establish connection!")
-		}
-		wg1.Done()
-	}()
-	wg1.Wait()
-	var wg2 sync.WaitGroup
-	wg2.Add(1)
-	go func() {
-		col.CollectorClient.Controller.Start(contractData)
 
-		wg2.Done()
-	}()
-	wg2.Wait()
+	c := client.New(reactor, contractData)
+	cc := &collector{
+		CollectorClient: *c,
+	}
+	c.Subscriber.Connect(ctx, addr, timeout)
+	if err != nil {
+		log.Error("failed to establish connection!")
+	}
+	c.Controller.Start(contractData)
+
 	log.Info("Invoking Subscriber")
-	col.CollectorClient.Subscriber.Subscribe(ctx, col.Reactor, contractData)
+	c.Subscriber.Subscribe(ctx, reactor, contractData)
+	return cc
 }
