c2ee9598b9a6ab371e39faec8d4a6f2277d75193
Daniel Turinn <turindani@gmail.com>
2023-09-21 12:59:56 +0300

    separating start from new

--- collector_client/collector_client.go
+++ collector_client/collector_client.go
@@ -20,18 +20,14 @@ type Collector interface {
 
 type collector struct {
 	CollectorClient client.Client
+	Reactor         reactor.Reactor[types.LogEvent, types.Callback]
 }
 
-func Start(addr string, timeout_duration int64) *collector {
+func New() *collector {
 	logger.CreateLoggerInstance()
 	log := logger.GetNamedLogger("collector_client")
 
 	defer logger.Sync()
-
-	if len(addr) == 0 {
-		log.Error("Please enter a valid websocket SOCKET_ADDRS in .env")
-		return nil
-	}
 
 	contract_config, err := os.Open("./config.json")
 	if err != nil {
@@ -65,22 +61,61 @@ func Start(addr string, timeout_duration int64) *collector {
 		_ = reactor.Start(ctx)
 	}()
 
+	c := client.New(reactor, contractData)
+	cc := &collector{
+		CollectorClient: *c,
+		Reactor:         reactor,
+	}
+	return cc
+}
+
+func (col *collector) Start(addr string, timeout_duration int64) {
+	logger.CreateLoggerInstance()
+	log := logger.GetNamedLogger("collector_client")
+
+	defer logger.Sync()
+
+	if len(addr) == 0 {
+		log.Error("Please enter a valid websocket SOCKET_ADDRS in .env")
+		return
+	}
+
+	contract_config, err := os.Open("./config.json")
+	if err != nil {
+		log.Sugar().Errorf("Could not open config.json: ", err)
+		panic(err)
+	}
+	defer contract_config.Close()
+
+	// Read the contents of the file
+	data, err := io.ReadAll(contract_config)
+	if err != nil {
+		log.Sugar().Errorf("Could not read config.json: ", err)
+		panic(err)
+	}
+
+	// Parse the JSON data into the ContractData struct
+	var contractData types.ContractData
+	err = json.Unmarshal(data, &contractData)
+	if err != nil {
+		log.Sugar().Errorf("Could not parse contract data from config.json: ", err)
+		panic(err)
+	}
+
+	ctx, cancel := context.WithCancel(context.Background())
+	defer cancel()
+
 	if timeout_duration == 0 {
 		timeout_duration = 100000 // default fallback timeout
 	}
 	timeout := time.Duration(timeout_duration) * time.Millisecond
 
-	c := client.New(reactor, contractData)
-	cc := &collector{
-		CollectorClient: *c,
-	}
-	c.Subscriber.Connect(ctx, addr, timeout)
+	col.CollectorClient.Subscriber.Connect(ctx, addr, timeout)
 	if err != nil {
 		log.Error("failed to establish connection!")
 	}
-	c.Controller.Start(contractData)
+	col.CollectorClient.Controller.Start(contractData)
 
 	log.Info("Invoking Subscriber")
-	c.Subscriber.Subscribe(ctx, reactor, contractData)
-	return cc
+	col.CollectorClient.Subscriber.Subscribe(ctx, col.Reactor, contractData)
 }
